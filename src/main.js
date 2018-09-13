851f9451c67046135f3b60ec476eb0e94fb8bc1a
Gure <Gurepaone@Gmail.com>
2018-09-13 19:26:03 +0800

    滴滴

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './router'
-import './cube-ui'
 import 'amfe-flexible'
+import './cube-ui'
 let store = require('./store')
 
 const router = new VueRouter({
